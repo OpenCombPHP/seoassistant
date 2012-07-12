d8151c4ce45e6ad800a1257ea9701d7de8eda9bc
aleechou <zq@alee2002.com>
2012-07-12 17:03:35 +0800

    create extension

---
+++ class/SeoAssistant.php
@@ -0,0 +1,15 @@
+<?php 
+namespace org\opencomb\seo\assistant ;
+
+use org\opencomb\platform\ext\Extension ;
+
+class SeoAssistant extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+		// todo ...
+	}
+}
